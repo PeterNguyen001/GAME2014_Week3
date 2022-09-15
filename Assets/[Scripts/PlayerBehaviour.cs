3b3929af125aaba219724d4bc2e1c3ac44e903c0
Peter_Nguyen <nguyenvietdung001@gmail.com>
2022-09-15 15:04:48 -0400

    added bound and collision

--- Assets/[Scripts/PlayerBehaviour.cs
+++ Assets/[Scripts/PlayerBehaviour.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
 
-    public float speed = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10;
+    public Boundary boundary;
+    public float verticalPosition;
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
-        float y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
-
-        transform.position = new Vector2(transform.position.x + x, transform.position.y + y);
+        Move();
     }
+
+    public void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+
+        transform.position += new Vector3(x, 0.0f, 0.0f);
+        float clampedPosition = Mathf.Clamp(transform.position.x, boundary.min, boundary.max);
+        transform.position = new Vector2(clampedPosition, verticalPosition);
+    }
+
+    
 }
