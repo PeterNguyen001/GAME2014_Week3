711e5da1c8bf02399d3519e06909c4a0ecfdfae6
Peter_Nguyen <nguyenvietdung001@gmail.com>
2022-09-22 14:49:58 -0400

    compelete touch

--- Assets/[Scripts/PlayerBehaviour.cs
+++ Assets/[Scripts/PlayerBehaviour.cs
@@ -8,21 +8,48 @@ public class PlayerBehaviour : MonoBehaviour
     public float speed = 10;
     public Boundary boundary;
     public float verticalPosition = -4;
+    private Camera camera;
+    public float verticalSpeed = 10.0f;
+    public bool usingMobileInput = false;
+
+    void Start()
+    {
+        camera =Camera.main;
+        usingMobileInput = Application.platform != RuntimePlatform.Android ||
+                      Application.platform != RuntimePlatform.IPhonePlayer;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (usingMobileInput)
+        {
+            MobileInput();
+        }
+
         Move();
     }
 
     public void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
-
-        transform.position += new Vector3(x, 0.0f, 0.0f);
         float clampedPosition = Mathf.Clamp(transform.position.x, boundary.min, boundary.max);
         transform.position = new Vector2(clampedPosition, verticalPosition);
     }
 
     
+
+    public void MobileInput()
+    {
+        foreach (var touch in Input.touches)
+        {
+            var destination = camera.ScreenToWorldPoint(touch.position);
+            transform.position = Vector2.Lerp(transform.position, destination, Time.deltaTime * verticalSpeed);
+        }
+    }
+
+    public void ConventionalInout()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        transform.position += new Vector3(x, 0.0f, 0.0f);
+    }
 }
